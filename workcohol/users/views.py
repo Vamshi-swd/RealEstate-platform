0a87752355a4dfcd3d4d0f7126794a7482eb1f1d
Vamshi-swd <akarapuvamshi123@gmail.com>
2025-02-13 15:40:45 +0530

    Update project with new features

--- workcohol/users/views.py
+++ workcohol/users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+# users/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            authenticate(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+def user_login(request):
+    return login(request, template_name='users/login.html', redirect_field_name='next')
+
+def user_logout(request):
+    return logout(request, next_page='home')
+
+def user_profile(request):
+    if request.user.is_authenticated:
+        return render(request, 'users/profile.html', {'user': request.user})
+    else:
+        return redirect('login')
