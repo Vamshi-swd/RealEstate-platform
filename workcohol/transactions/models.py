1ef65fc2cc28a7601c1320f961cbd6370bb01b58
Bharath552-bit <patthembharath32@gmail.com>
2025-02-18 15:40:31 +0530

    models foe transactions

--- workcohol/transactions/models.py
+++ workcohol/transactions/models.py
@@ -1,13 +1,12 @@
+# transactions/models.py
 from django.db import models
 from users.models import User
-from properties.models import Property
 
 class Transaction(models.Model):
-    property = models.ForeignKey(Property, on_delete=models.CASCADE)
-    buyer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transactions')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=20, choices=[('pending', 'Pending'), ('completed', 'Completed')])
+    description = models.TextField()
     transaction_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Transaction for {self.property.title} by {self.buyer.username}"
+        return f"Transaction by {self.user.username} for ${self.amount} on {self.transaction_date}"
