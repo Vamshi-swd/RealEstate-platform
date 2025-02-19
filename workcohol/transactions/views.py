c05ed8627754fcdb383bc6051b5e0560aa57f203
Bharath552-bit <patthembharath32@gmail.com>
2025-02-19 20:40:01 +0530

    more

--- workcohol/transactions/views.py
+++ workcohol/transactions/views.py
@@ -1,5 +1,5 @@
 # transactions/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Transaction
 from .forms import TransactionForm
